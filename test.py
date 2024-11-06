6abb165efb76b95eecf11e47747525b3251501c9
jiaohl <jiaohailong28@gmail.com>
2024-11-06 19:59:59 +0800

    second commit

--- test.py
+++ test.py
@@ -3,11 +3,15 @@ import re
 def remove_extra_newlines(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
+    
+    matchs = re.findall('^(####\s)\*\*([^*]+)\*\*$', content, flags=re.MULTILINE)
 
+    for match in matchs:
+        print(f"Match Line : {match[0]}{match[1]}")
 
     # formatted_content = re.sub(r'(?<=:|\)|{|>)\n{2,}', '\n', content)
     # formatted_content = re.sub(r'\s*\n{2,}(?=\s*}|{)', '\n', content)
-    formatted_content = re.sub(r'^(### )\*\*([^*]+)\*\*$', r'\1\2', content, flags=re.MULTILINE)
+    formatted_content = re.sub(r'^(####\s)\*\*([^*]+)\*\*$', r'\1\2', content, flags=re.MULTILINE)
 
     with open(file_path, 'w') as file:
         file.write(formatted_content)
